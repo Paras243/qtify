913e5581f2bf118683584dc84f6c8c60b10aabe9
Paras_Raina <parasr243@gmail.com>
2024-10-12 17:09:14 +0530

    kjdv

--- src/components/Carousel/CarouselRightNavigation/CarouselRightNavigation.js
+++ src/components/Carousel/CarouselRightNavigation/CarouselRightNavigation.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+/*import React, { useEffect } from "react";
 import { useSwiper } from "swiper/react";
 import { useState } from "react";
 import styles from "../Carousel.module.css";
@@ -23,4 +23,32 @@ const CarouselRightNavigation = () => {
 	);
 };
 
+export default CarouselRightNavigation;*/
+
+import React, { useEffect, useState } from "react";
+import { useSwiper } from "swiper/react";
+import styles from "../Carousel.module.css";
+import { ReactComponent as RightArrow } from "../../../assets/images/RightArrow.svg";
+
+const CarouselRightNavigation = () => {
+	const swiper = useSwiper();
+	const [isEnd, setIsEnd] = useState(swiper.isEnd);
+
+	useEffect(() => {
+		const updateIsEnd = () => {
+			setIsEnd(swiper.isEnd); // Track if we are at the end
+		};
+
+		swiper.on("slideChange", updateIsEnd);
+		return () => swiper.off("slideChange", updateIsEnd); // Cleanup listener
+	}, [swiper]);
+
+	return (
+		<div className={styles.rightNavigation}>
+			{!isEnd && <RightArrow onClick={() => swiper.slideNext()} />}
+		</div>
+	);
+};
+
 export default CarouselRightNavigation;
+
